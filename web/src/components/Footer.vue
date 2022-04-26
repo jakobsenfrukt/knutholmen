22947514ad30ade7dc21e2f08d8b67646749b6d6
elisejakob <hei@elisejakob.no>
2022-04-26 23:30:12 +0200

    improve footer

--- web/src/components/Footer.vue
+++ web/src/components/Footer.vue
@@ -4,17 +4,23 @@
       <g-link :to="$tp('/')" class="logo"><Logo /></g-link>
     </div>
     <ul class="contact-list">
+      <li>
+        <h2>{{ $t("headings.address") }}</h2>
+      </li>
       <li v-if="$static.contact.address">
         <block-content
           :blocks="$static.contact.address._rawNo"
           v-if="$static.contact.address._rawNo && $context.locale == 'no'"
-          class="block-content"
         />
         <block-content
           :blocks="$static.contact.address._rawEn"
           v-else-if="$static.contact.address._rawEn && $context.locale == 'en'"
-          class="block-content"
         />
+      </li>
+    </ul>
+    <ul class="contact-list">
+      <li>
+        <h2>{{ $t("headings.contact") }}</h2>
       </li>
       <li v-if="$static.contact.email">
         <a
@@ -44,16 +50,16 @@
         >
       </li>
     </ul>
-    <Button type="giftcard" />
-    <ul class="">
-      <li><img src="/assets/images/footerlogos/logo-en.png" /></li>
-      <li><img src="/assets/images/footerlogos/dehistoriske.svg" /></li>
-    </ul>
+    <div class="giftcard">
+      <Button type="giftcard" />
+    </div>
     <ul class="footer-logos">
+      <li><img src="/assets/images/footerlogos/dehistoriske.svg" /></li>
+      <li><img src="/assets/images/footerlogos/logo-en.png" /></li>
+    </ul>
+    <ul class="footer-logos footer-logos--visa">
       <li><img src="/assets/images/footerlogos/VISA__Verified_by.png" /></li>
       <li><img src="/assets/images/footerlogos/MasterCard.png" /></li>
-      <li><img src="/assets/images/footerlogos/logo-en.png" /></li>
-      <li><img src="/assets/images/footerlogos/dehistoriske.svg" /></li>
     </ul>
   </footer>
 </template>
@@ -90,15 +96,17 @@ export default {
 <style lang="scss" scoped>
 .site-footer {
   background: var(--color-blue-dark);
-  color: var(--color-yellow);
+  color: var(--color-white);
   width: 100%;
   display: grid;
-  grid-template-columns: 1fr 1fr;
-  gap: calc(var(--spacing-sitepadding) * 2);
-  padding: var(--spacing-sitepadding);
+  grid-template-columns: repeat(4, 1fr);
+  column-gap: calc(var(--spacing-sitepadding) * 2);
+  row-gap: var(--spacing-sitepadding);
+  padding: var(--spacing-sitepadding) var(--spacing-sitepadding) 0;
 
   .logo-wrapper {
     grid-column: 1 / -1;
+    margin-bottom: 3rem;
   }
   .logo {
     display: inline-block;
@@ -106,10 +114,13 @@ export default {
   a {
     color: inherit;
   }
+  h2 {
+    font-size: var(--font-size-l);
+  }
 }
 .contact-list {
   list-style: none;
-  margin: 0;
+  margin: 0 0 6rem;
   padding: 0;
 }
 .footer-logos {
@@ -120,7 +131,20 @@ export default {
   display: flex;
   align-items: center;
   gap: 2rem;
+
+  li {
+    height: 3rem;
+    img {
+      height: 100%;
+      filter: grayscale(0.3);
+    }
+  }
+}
+.footer-logos--visa {
   background: white;
+  margin: 0 calc(var(--spacing-sitepadding) * -1);
+  padding: calc(var(--spacing-sitepadding) / 4) var(--spacing-sitepadding);
+  justify-content: flex-end;
 
   li {
     height: 2rem;
@@ -132,7 +156,7 @@ export default {
   }
 }
 
-@media (max-width: 1000px) {
+@media (max-width: 800px) {
   .site-footer {
     grid-template-columns: 1fr;
   }
